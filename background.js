5b56279c7adb7d98bdf1d9260567ce573b3e2699
Shrey A <Shreystechtips@gmail.com>
2020-05-18 17:24:24 -0700

    improvement in yt load event

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
   chrome.tabs.executeScript(null, { file: "secretvars.js" });
   chrome.tabs.executeScript(null, { file: "yt.js" });
 });
